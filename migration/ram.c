1d7cb10f2cd79291d77e6b18e7c6bdb2f937c1ac
Tobias Zagorni <tobias@zagorni.eu>
2021-08-24 14:13:36 +0200

    ignore RAM in migration

--- migration/ram.c
+++ migration/ram.c
@@ -3026,5 +3026,6 @@ static SaveVMHandlers savevm_ram_handlers = {
 void ram_mig_init(void)
 {
     qemu_mutex_init(&XBZRLE.lock);
-    register_savevm_live(NULL, "ram", 0, 4, &savevm_ram_handlers, &ram_state);
+    //register_savevm_live(NULL, "ram", 0, 4, &savevm_ram_handlers, &ram_state);
+    assert(&savevm_ram_handlers != NULL);
 }
